c2ac737111c9ebba3f81020e0dfb63ce9dfa6341
MrZend <vlad04102002@gmail.com>
2020-11-29 19:37:26 +0200

    Fix Bug

--- lab9/task10/Task10.java
+++ lab9/task10/Task10.java
@@ -3,24 +3,26 @@ package lab9.task10;
 public class Task10 {
     public static void main(String[] args) {
         printResult("The user with the nickname koala757677 this month wrote 3 times more comments than the user with the nickname croco181dile920 4 months ago");
+        printResult("% ^ %");
+        printResult("     ");
     }
 
     private static String toUpperCaseWords(String str){
         String[] subStr;
-        boolean digit = true;
+        boolean isWord = true;
         String result = "";
         subStr = str.trim().split(" ");
         for (int i = 0; i < subStr.length; i++) {
             for (int j = 0; j < subStr[i].length(); j++) {
                 if(Character.isDigit(subStr[i].charAt(j))){
-                    digit = false;
+                    isWord = false;
                     break;
                 }
             }
-            if(digit){
+            if(isWord){
                 subStr[i] = subStr[i].toUpperCase();
             }
-            digit = true;
+            isWord = true;
             result = result + subStr[i] + " ";
         }
         return result.trim();
